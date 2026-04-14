f8123c1fe5179c4d3b78314135855ce751a6dae8
D Cowman <G00474276@atu.ie>
2026-04-14 15:45:43 +0100

    installed & set up ionic storage. generated services MyHttp & Data. initialised storage. Repurposed and inserted search method written for Ex10

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton } from '@ionic/angular/standalone';
+import { Data } from '../services/data';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
-})
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton,FormsModule],
+  })
+
 export class HomePage {
-  constructor() {}
+  newSearch:string = "";
+  keywordToSearch:string ="";
+
+  constructor(private mds:Data, private router: Router) {}
+
+  ngOnInit() {
+    
+  }
+
+  async setKeyword(){
+    await this.mds.set("keyword", this.newSearch);
+    this.keywordToSearch = this.newSearch;
+    this.router.navigate(['/movies']);
+  }
 }
